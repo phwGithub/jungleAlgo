687c9a50eaf31123d765659b74a799e53c76fe70
phwGithub <yey6689@gmail.com>
2022-10-31 21:42:58 +0900

    # 백준 9663 N-Queen

---
+++ hyeonwoopark/WEEK01/9663.py
@@ -0,0 +1,26 @@
+nQueenCnt = 0
+n = int(input())
+board = [-1 for i in range(n)]
+
+def check(x):
+    for i in range(x):
+        if board[x] == board[i] or abs(board[x] - board[i]) == abs(x - i):
+            return False
+    
+    return True
+
+def findNqueenCnt(idx):
+    global nQueenCnt
+
+    if idx == n:
+        nQueenCnt += 1
+        return
+    
+    for i in range(n):
+        board[idx] = i
+
+        if check(idx):
+            findNqueenCnt(idx + 1)
+
+findNqueenCnt(0)
+print(nQueenCnt)
